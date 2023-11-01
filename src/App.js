ad3e889f640fef10eef0ae8460bafe5470c8b249
Mazouz-Firdaous <mzfirdaws1@gmail.com>
2023-11-01 09:47:30 +0100

    my first app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import "./App.css";
-import Calcul from "./component/Calcul";
-import Count from "./component/Count";
-import Explorer from "./component/Explorer";
-import Slides from "./component/Slides";
-import Inscription from "./component/Inscription";
-import Tpslider from "./component/TPslider";
 
 function App(){
   return(
   <div className="App">
-    {/*<Explorer/>*/}
-    <br />
-    <Count/>
-    <br />
-    <Tpslider/>
-    <br />
-    {/*<Inscription/>*/}
+    
   </div>
 
   );
